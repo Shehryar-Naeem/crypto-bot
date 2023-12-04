389e33253e96f339c03e96e49123b559113b6b28
shehryar <shehryarkashan@gmail.com>
2023-12-04 20:51:58 +0500

    change

--- src/pages/TermsConditions/index.jsx
+++ src/pages/TermsConditions/index.jsx
@@ -9,8 +9,6 @@ const TermsConditions = () => {
     Welcome to the intriguing realm of cryptocurrencies, where digital assets and blockchain technology collide to shape the future of finance. This beginner's guide aims to demystify the complexities surrounding cryptocurrencies, offering a comprehensive overview for those taking their first steps into this revolutionary landscape.Cryptocurrencies are digital or virtual currencies that utilize cryptography for security and operate on a decentralized network called blockchain. Blockchain technology, the backbone of cryptocurrencies, ensures transparency, immutability, and security by decentralizing data storage and processing.Bitcoin, the pioneer of cryptocurrencies, paved the way for a multitude of alternative digital coins known as altcoins. Ethereum, Ripple, Litecoin, and others have unique features and use cases, contributing to the diverse crypto ecosystem.Setting up a wallet is the first step on your crypto journey. Wallets come in various forms, such as hardware, software, paper, or mobile. Understanding private keys, seed phrases, and choosing a reputable exchange are crucial aspects for beginners.
     </p>
     <BackBtn/>
-
-    
 </section>
   )
 }
