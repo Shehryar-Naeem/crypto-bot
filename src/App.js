2357f860091919737d89496fc0fc6a6348323544
shehryar <shehryarkashan@gmail.com>
2023-12-04 20:14:23 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
-import './App.css';
 
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CryptoGuide from './pages/CryptoGuide';
+import ExclusiveDeals from './pages/ExclusiveDeals';
+import Membership from './pages/Membership';
+import ModelFindr from './pages/ModelFindr';
+import RequestModel from './pages/RequestModel';
+import Support from './pages/Support';
+import TermsConditions from './pages/TermsConditions';
 const telegram = window.Telegram.WebApp
 
 function App() {
@@ -55,12 +60,12 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/CryptoGuide' element={<CryptoGuide/>}/>
-          <Route path='/ExclusiveDeals' element={<CryptoGuide/>}/>
-          <Route path='/Membership' element={<CryptoGuide/>}/>
-          <Route path='/ModelFindr' element={<CryptoGuide/>}/>
-          <Route path='/RequestModel' element={<CryptoGuide/>}/>
-          <Route path='/Support' element={<CryptoGuide/>}/>
-          <Route path='/TermsConditions' element={<CryptoGuide/>}/>
+          <Route path='/ExclusiveDeals' element={<ExclusiveDeals/>}/>
+          <Route path='/Membership' element={<Membership/>}/>
+          <Route path='/ModelFindr' element={<ModelFindr/>}/>
+          <Route path='/RequestModel' element={<RequestModel/>}/>
+          <Route path='/Support' element={<Support/>}/>
+          <Route path='/TermsConditions' element={<TermsConditions/>}/>
         </Routes>
       </section>
     </>
