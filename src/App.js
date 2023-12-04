86f24fb123b09a0259cd2e976d12322e70f09305
shehryar <shehryarkashan@gmail.com>
2023-12-04 16:39:17 +0500

    change

--- src/App.js
+++ src/App.js
@@ -12,31 +12,34 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 const telegram = window.Telegram.WebApp
 function App() {
-  useEffect(() => {   
-    telegram.ready();
+  useEffect(() => { 
+    function setThemeClass() {
+     document.body.className = telegram.colorScheme;
+     
+  }
+
+  telegram.onEvent('themeChanged', setThemeClass);
+  setThemeClass();
   })
   return (
     <div className='app_container'>
       <div className='parent_menu_container'>
         <div className='parent_submenu_container'>
-
-
-          x
           <div className='child_menu_container'>
             <ul className='menu'>
-              <li className='menu-item'><Link to='/'><span className='icon'><TbHandClick className='menu-icon' /></span>How to use crypto</Link></li>
+              <li className='menu-item'><Link to='/'><span className='icon'><TbHandClick className='menu-icon' /></span>What is ModelFindr?</Link></li>
 
-              <li className='menu-item'><span className='icon'><MdOutlineContactSupport className='menu-icon' /></span><Link to='/'>Support</Link></li>
+              <li className='menu-item'><span className='icon'><MdOutlineContactSupport className='menu-icon' /></span><Link to='/'>How to request a model</Link></li>
 
-              <li className='menu-item'><span className='icon'><BsFillFileRuledFill  className='menu-icon' /></span><Link to='/'>Terms and conditions</Link></li>
+              <li className='menu-item'><span className='icon'><BsFillFileRuledFill  className='menu-icon' /></span><Link to='/'>Membership</Link></li>
 
-              <li className='menu-item'><span className='icon'><TfiWrite className='menu-icon' /></span><Link to='/'>Proposals</Link></li>
+              <li className='menu-item'><span className='icon'><TfiWrite className='menu-icon' /></span><Link to='/'>Support</Link></li>
 
-              <li className='menu-item'><span className='icon'><SiBoost className='menu-icon' /></span><Link to='/'>Boosts</Link></li>
+              <li className='menu-item'><span className='icon'><SiBoost className='menu-icon' /></span><Link to='/'>Exclusive Deals</Link></li>
 
-              <li className='menu-item'><span className='icon'><MdUnsubscribe className='menu-icon' /></span><Link to='/'>Subscription</Link></li>
+              <li className='menu-item'><span className='icon'><MdUnsubscribe className='menu-icon' /></span><Link to='/'>Crypto Beginner Guide</Link></li>
 
-              <li className='menu-item'><span className='icon'><IoWalletSharp className='menu-icon' /></span><Link to='/'>Wallet</Link></li>
+              <li className='menu-item'><span className='icon'><IoWalletSharp className='menu-icon' /></span><Link to='/'>Terms and Conditions</Link></li>
 
               
             </ul>
