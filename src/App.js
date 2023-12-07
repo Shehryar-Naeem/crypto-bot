fe46f3b582afcf5f465ffe731da2bd72d79f384c
shehryar <shehryarkashan@gmail.com>
2023-12-07 11:47:35 +0500

    change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ModelFindr from './pages/ModelFindr';
 import RequestModel from './pages/RequestModel';
 import Support from './pages/Support';
 import TermsConditions from './pages/TermsConditions';
+import Header from './Component/header';
 const telegram = window.Telegram.WebApp
 
 function App() {
@@ -27,36 +28,10 @@ function App() {
   setThemeClass();
   })
   return (
-    // <div className='app_container'>
-    //   <div className='parent_menu_container'>
-    //     <div className='parent_submenu_container'>
-    //       <div className='child_menu_container'>
-    //         <ul className='menu'>
-    //           <li className='menu-item'><Link to='/'><span className='icon'><TbHandClick className='menu-icon' /></span>What is ModelFindr?</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><MdOutlineContactSupport className='menu-icon' /></span><Link to='/'>How to request a model</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><BsFillFileRuledFill  className='menu-icon' /></span><Link to='/'>Membership</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><TfiWrite className='menu-icon' /></span><Link to='/'>Support</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><SiBoost className='menu-icon' /></span><Link to='/'>Exclusive Deals</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><MdUnsubscribe className='menu-icon' /></span><Link to='/'>Crypto Beginner Guide</Link></li>
-
-    //           <li className='menu-item'><span className='icon'><IoWalletSharp className='menu-icon' /></span><Link to='/'>Terms and Conditions</Link></li>
-
-              
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </div>
-
-
-    // </div>
+    
     <>
       <section>
-
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/CryptoGuide' element={<CryptoGuide/>}/>
